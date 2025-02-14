61855fcdb1e18b771f2e49a70febe6c6fc793085
Prateek-rajput-007 <prateekrajput47@yahoo.com>
2025-02-14 15:48:03 +0530

    initial commit

--- next.config.js
+++ next.config.js
@@ -4,6 +4,12 @@ const nextConfig = {
    swcMinify:true,
    experimental:{
       appDir:true
+   },
+   env: {
+      // Add your environment variables here
+      MONGODB_URI: process.env.MONGODB_URI,
+      NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+      // ...other environment variables
    }
 }
 
